5db801ebaf1048b5f1ba053a4cf95dc5596fc9b0
Luiza029 <cwluiza029@gmail.com>
2026-09-25 15:52:35 -0300

    cria TipoSinistro

--- src/br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
@@ -1,18 +1,32 @@
 package br.edu.cs.poo.ac.seguro.entidades;
-
-
-/*
- * Implementar um enum com as seguintes constantes:
- *
- * 	COLISAO(1,"Colisão"),
-	INCENDIO(2,"Incêndio"),
-	FURTO(3, "Furto"),
-	ENCHENTE(4, "Enchente"),
-	DEPREDACAO(5, "Depredação");
- *
- * O enum deve ter construtor privado, métodos get públicos para os atributos codigo e nome,
- * e um método público e estático TipoSinistro getTipoSinistro(int codigo), que
- * retorna o tipo de sinistro correspondente ao código recebido como parâmetro
- */
 public enum TipoSinistro {
+    COLISAO(1, "Colisão"),
+    INCENDIO(2, "Incêndio"),
+    FURTO(3, "Furto"),
+    ENCHENTE(4, "Enchente"),
+    DEPREDACAO(5, "Depredação");
+
+    private int codigo;
+    private String nome;
+
+    private TipoSinistro(int codigo, String nome){
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public static TipoSinistro getTipoSinistro(int codigo){
+        for(TipoSinistro c : TipoSinistro.values())
+            if (c.getCodigo() == codigo) {
+                return c;
+            }
+        return null;
+    }
 }
